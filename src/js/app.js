b39ffbf3d6def8f97a95c3cc6e9cdcc214518909
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2017-04-18 15:23:31 +0200

    add searching capability using input form

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,41 @@
-console.log("Hello from app.js");
+var galleryData = {
+};
+
+var galleryItems = $(".gallery-item");
+
+for (var i = 0; i < galleryItems.length; i++){
+    galleryData[i] = {
+            source: "source "+i.toString(),
+            caption: "caption "+i.toString(),
+        };
+}
+
+for (var key in galleryData) {
+}
+
+// filter the image search box
+var filterImage = function(){
+    var searchText = $('#search-box').val().toLowerCase();
+    
+    $(".gallery-item a").each(function(index) {
+        var href = $(this).attr("href").toLowerCase();
+        var caption = $(this).attr("data-caption").toLowerCase();
+        var alt = $(this).children().attr("alt").toLowerCase();
+
+        if (href.includes(searchText) || caption.includes(searchText) || alt.includes(searchText)){
+            $(this).parent().show();
+        } else {
+            $(this).parent().hide();
+        }
+    });
+};
+
+// event handlers
+
+$('.gallery-item a').click(function(event){
+    event.preventDefault();
+    console.log(event);
+    console.log(this);
+});
+
+$('#search-box').keyup(filterImage);
